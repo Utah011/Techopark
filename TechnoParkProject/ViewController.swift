6e90bfe55a44f5e9ca66b580b009898f67049ca4
Roman Kovayin <kovajkin2012@yandex.ru>
2020-03-02 20:28:54 +0300

    Initial Commit

---
+++ TechnoParkProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnoParkProject
+//
+//  Created by Роман Ковайкин on 02.03.2020.
+//  Copyright © 2020 Роман Ковайкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
